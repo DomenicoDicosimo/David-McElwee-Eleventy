d045582a2d0559b84dc8a14bc5e88928e4c8d18b
DomenicoDicosimo <65661974+DomenicoDicosimo@users.noreply.github.com>
2023-03-04 18:00:33 -0500

    Removed Excess

--- src/assets/js/fvf.js
+++ src/assets/js/fvf.js
@@ -1,7 +1,6 @@
 const faqitems = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
-  console.log('Worked');
   const itemToggle = this.getAttribute('aria-expanded');
   for (i = 0; i < faqitems.length; i++) {
         faqitems[i].setAttribute('aria-expanded', 'false');
